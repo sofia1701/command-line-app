1d0951e5b547c698ef6bfbab26965459b7db6e75
sofia1701 <sofia.dionisio94@gmail.com>
2024-02-03 21:19:12 +0000

    Initial setup and logic to run app

---
+++ README.md
@@ -0,0 +1,31 @@
+# Node Js App to count repeated words in text files.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Prerequisites
+
+Clone the repository
+
+Then cd into the repository.
+
+## Installing
+
+Run `npm install` to install the necessary libraries required for Node.js to run.
+
+## Run
+
+To run the application, open terminal and run the following command:
+
+run-func index.js fileReader "/Users/sofiadionis/Documents/textFile1.txt"
+
+index.js is the name of the file containing the fuction fileReader, which takes the file path as an argument as shown above.
+
+## Built With
+
+Javascript
+
+## Authors
+
+Sofia Dionisio
---
+++ index.js
@@ -0,0 +1,32 @@
+import { readFile } from "fs";
+
+const fileReader = (filePath) => {
+  readFile(filePath, "utf8", (err, text) => {
+    if (err) throw err;
+
+    //Object
+    const wordCountObject = text
+      .replace(/[.,]/g, "")
+      .split(/\s/)
+      .reduce(
+        (map, word) =>
+          Object.assign(map, {
+            [word]: map[word] ? map[word] + 1 : 1,
+          }),
+        {}
+      );
+
+    //Convert object to array
+    const wordCountArray = Object.entries(wordCountObject);
+
+    //Sort array based on number of occurrences
+    wordCountArray.sort((a, b) => b[1] - a[1]);
+
+    //Convert back to object
+    const sortedWordCountObject = Object.fromEntries(wordCountArray);
+
+    console.log(sortedWordCountObject);
+  });
+};
+
+export { fileReader };
